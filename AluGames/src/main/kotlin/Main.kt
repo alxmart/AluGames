5136076900b0501cbee1fe5bc1cca5ba18bb91eb
alxmart <alxmartinez@gmail.com>
2023-12-30 11:18:44 -0300

    Testing installation on a new machine.

--- AluGames/src/main/kotlin/Main.kt
+++ AluGames/src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ import java.net.http.HttpResponse.BodyHandlers
 
 fun main() {
 
-    val client: HttpClient = HttpClient.newHttpClient()
+    val client: java.net.http.HttpClient = java.net.http.HttpClient.newHttpClient()
 
     val request = HttpRequest.newBuilder()
         .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
@@ -16,5 +16,4 @@ fun main() {
 
     val json = response.body()
     println(json)
-
 }
